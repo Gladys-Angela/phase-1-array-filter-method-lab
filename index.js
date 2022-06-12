4f5de5c3d9d3cf9411d9bb747a1a1de12541d18c
Gladys <gladysangelakamau@gmail.com>
2022-06-12 16:38:33 +0300

    arrayfiltermethod

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching (drivers,Bobby ) {
+    return drivers.filter(function (driverName) {
+      return driverName.toLowerCase() === Bobby.toLowerCase();
+    });
+  }
+  function fuzzyMatch (drivers, Sa) {
+    let lengthOfName = Sa.length;
+    return drivers.filter(function (driverName) {
+      return driverName.slice(0, lengthOfName) === Sa;
+    });
+  }
+  function matchName(drivers, Bobby){
+    return drivers.filter((Pittsburgh) => Pittsburgh.name === Bobby);
+
+  }
